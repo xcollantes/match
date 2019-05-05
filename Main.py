7534388d6027f730f839f71028fd89f782accd02
Cloud User <ec2-user@ip-172-31-43-107.us-west-2.compute.internal>
2019-05-05 22:44:10 +0000

    First commit

---
+++ Main.py
@@ -0,0 +1,18 @@
+@author "Xavier Collantes"
+@date May 5 2019
+
+
+import logging
+import Match
+
+logging.simpleConfig(level=logging.INFO)
+
+
+def main():
+  logging.DEBUG("Starting MAIN.")
+
+
+
+
+if __name__="__main__":
+  main()
